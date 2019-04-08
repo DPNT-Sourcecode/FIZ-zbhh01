b0541f266d374073372cf182aa45cac05ee41a2e
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 13:50:49 +0000

    Mon Apr 08 13:50:49 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,22 +3,28 @@ class FizzBuzz
   def fizz_buzz(number)
     isFuzzBuzz?(number)
 
-    if (number % 3).zero? || (number.to_s.include? '3')
-      'fizz'
-    elsif (number % 5).zero? || (number.to_s.include? '5')
-      'buzz'
-    else
-      number.to_s
-    end
+    # elsif (number % 3).zero? || (number.to_s.include? '3')
+    #   'fizz'
+    # elsif (number % 5).zero? || (number.to_s.include? '5')
+    #   'buzz'
+    # else
+    #   number.to_s
+    # end
   end
 
   def isFuzzBuzz?(number)
     p number
-    if (number % 15).zero?)
-      if ((number.to_s.include? '3') && (number.to_s.include? '5'))
-      'fizz buzz'
+    if (number % 15).zero?
+      p 'dividable by 15'
+      if (number.to_s.include? '3')
+        p 'contains 3'
+      if (number.to_s.include? '5')
+        p 'contains 5'
+       return 'fizz buzz'
     end
     end
   end
 end
+end
+
 
