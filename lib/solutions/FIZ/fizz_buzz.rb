775aa53ee6c552da6e9b4598833b204899302fd7
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 13:59:49 +0000

    Mon Apr 08 13:59:49 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,30 +1,31 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
   def fizz_buzz(number)
-    if isFuzzBuzz?(number)
+    if is_fizz_buzz?(number)
       'fizz buzz'
-    elsif isFizz?(number)
+    elsif is_fizz?(number)
       'fizz'
-    elsif isBuzz?(number)
+    elsif is_buzz?(number)
       'buzz'
     else
       number.to_s
     end
   end
 
-  def isFuzzBuzz?(number)
+  def is_fuzz_buzz?(number)
     (number % 15).zero? ||
-      (number.to_s.include? '3') && (number.to_s.include? '5')
+    (number.to_s.include? '3') && (number.to_s.include? '5')
   end
 
-  def isFizz?(number)
+  def is_fizz?(number)
     (number % 3).zero? || (number.to_s.include? '3')
   end
 
-    def isBuzz?(number)
-      (number % 5).zero? || (number.to_s.include? '5')
-    end
+  def is_buzz?(number)
+    (number % 5).zero? || (number.to_s.include? '5')
   end
+end
+
 
 
 
