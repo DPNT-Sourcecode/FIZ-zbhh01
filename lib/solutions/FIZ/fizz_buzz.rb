d46f38290bdeeace173abd96d524a0fbc786f1aa
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 15:09:43 +0000

    Mon Apr 08 15:09:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,7 +3,7 @@ class FizzBuzz
   def fizz_buzz(number)
     # fbstring = fizz(number) + buzz(number)
     fbstring = check_fb(3,number,'fizz')
-    fbstring = check_fb(5,number,' buzz')
+    fbstring += check_fb(5,number,' buzz')
     fbstring.strip!
     if fbstring.length.zero?
       number.to_s
@@ -11,29 +11,12 @@ class FizzBuzz
       fbstring
     end
   end
-
-  def fizz(number)
-    if (number % 3).zero? || (number.to_s.include? '3')
-      'fizz'
-    else
-      ' '
-    end
-  end
-  def buzz(number)
-    if (number % 5).zero? || (number.to_s.include? '5')
-      ' buzz'
-    else
-      ' '
-    end
-  end
   def check_fb(check_number, input_number, output_string)
-    p check_number
-    p input_number
-    p output_string
     if (input_number % check_number).zero? || (input_number.to_s.include? check_number.to_s)
-      p output_string
+      output_string
     else
       ' '
     end
   end
 end
+
