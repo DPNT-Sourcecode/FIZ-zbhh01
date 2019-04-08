6521e6348f19258a7f6574dab9eadcface577a7c
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:07:01 +0000

    Mon Apr 08 14:07:01 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,6 +1,11 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
+
+  attr_reader:fizz_num, buzz_num
+
   def fizz_buzz(number)
+    fizz_str = nil
+    buzz_str = nil
     if fizz_buzz?(number)
       'fizz buzz'
     elsif fizz?(number)
@@ -18,13 +23,15 @@ class FizzBuzz
   end
 
   def fizz?(number)
-    (number % 3).zero? || (number.to_s.include? '3')
+    if (number % 3).zero? || (number.to_s.include? '3')
+     fizz_num = 1
   end
 
   def buzz?(number)
     (number % 5).zero? || (number.to_s.include? '5')
   end
 end
+
 
 
 
