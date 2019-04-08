867aeb0fbd1d641d393493cc7693138dcc0edd78
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 15:48:43 +0000

    Mon Apr 08 15:48:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,10 +1,9 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
   def fizz_buzz(number)
-    get_fb_string(number)
-    fbstring = check_fb(3, number, 'fizz')
-    fbstring += check_fb(5, number, ' buzz')
-    fbstring.strip!
+    p number
+    fbstring = get_fb_string(number)
+    p fbstring
     if number > 10
       fbstring += check_same_num(number.to_s[0].to_i,number)
     end
@@ -16,6 +15,12 @@ class FizzBuzz
     end
   end
 
+  def get_fb_string(number)
+    fbstring = check_fb(3, number, 'fizz')
+    fbstring += check_fb(5, number, ' buzz')
+    fbstring.strip!
+  end
+
   def check_fb(check_number, input_number, output_string)
     if (input_number % check_number).zero? || (input_number.to_s.include? check_number.to_s)
       output_string
@@ -23,6 +28,7 @@ class FizzBuzz
       ' '
     end
   end
+
   def check_same_num(check_number, input_number)
     x  = input_number.to_s.length
     y = input_number.to_s.count check_number.to_s
@@ -32,12 +38,7 @@ class FizzBuzz
       ' '
     end
   end
-
-  def get_fb_string(number)
-    fbstring = check_fb(3, number, 'fizz')
-    fbstring += check_fb(5, number, ' buzz')
-    fbstring.strip!
-  end
 end
+
 
 
