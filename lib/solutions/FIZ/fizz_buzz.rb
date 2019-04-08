de65d5ebd6c795d07a0e014682977b622cd446e0
sbx_user1051 <sbx_user1051@169.254.247.213>
2019-04-08 20:46:41 +0000

    Mon Apr 08 20:46:41 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,20 +1,14 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
-  attr_reader:
-  :fizz_num,
-  :buzz_num,
-  :fizz_marker,
-  :buzz_marker,
-  :deluxe_marker,
-  :fake_marker,
-  :fizz_str,
-  :buzz_str,
-  :deluxe_str,
-  :fake_str
+  attr_reader :fizz_marker, :buzz_marker, :deluxe_marker, :fake_marker
+  FIZZ_NUM = 3
+  BUZZ_NUM = 5
+  FIZZ_STR = 'fizz'
+  BUZZ_STR = 'buzz'
+  DELUXE_STR = 'deluxe'
+  FAKE_STR = 'fake'
 
   def initialize
-    fizz_num = 3
-    buzz_num = 5
     fizz_marker = false
     buzz_marker = false
     deluxe_marker = false
@@ -27,13 +21,13 @@ class FizzBuzz
 
   def fizz_buzz(number)
     # check fizz
-    fizz_marker = check_fizz_buzz(fizz_num, number)
+    fizz_marker = check_fizz_buzz(FIZZ_NUM, number)
     if fizz_marker
-      deluxe_marker = check_deluxe(fizz_num,number)
+      deluxe_marker = check_deluxe(FIZZ_NUM,number)
     end
-    buzz_marker = check_fizz_buzz(buzz_num, number)
+    buzz_marker = check_fizz_buzz(BUZZ_NUM, number)
     if !deluxe_marker
-      deluxe_marker = check_deluxe(buzz_marker, number)
+      deluxe_marker = check_deluxe(BUZZ_NUM, number)
       if deluxe_marker
         fake_marker = check_fake(number)
       end
@@ -57,17 +51,18 @@ class FizzBuzz
   def make_string
     output_str = ''
     if fizz_marker
-      output_str += fizz_str
+      output_str += FIZZ_STR
     end
     if buzz_marker
-      output_str += ' ' +  buzz_str
+      output_str += ' ' +  BUZZ_STR
     end
     if deluxe_marker
-      output_str += ' ' + deluxe_str
+      output_str += ' ' + DELUXE_STR
     end
     if fake_marker
-      fake_str += ' ' + buzz_str
+      fake_str += ' ' + FAKE_STR
     end
     output_str
   end
 end
+
