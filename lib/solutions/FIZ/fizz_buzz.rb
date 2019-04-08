9f12dd5d3d741a6343219ff05a99c2c8d880476b
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:46:01 +0000

    Mon Apr 08 14:46:01 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,10 +4,7 @@ class FizzBuzz
   attr_reader :fizz_str, :buzz_str
 
   def fizz_buzz(number)
-    fbstring =
-
-    fbstring = fizz(number)
-    fbstring += ' ' + buzz(number)
+    fbstring = fizz(number) + buzz(number)
     fbstring.strip!
     if fbstring.length == 0
       number.to_s
@@ -20,16 +17,17 @@ class FizzBuzz
     if (number % 3).zero? || (number.to_s.include? '3')
      'fizz'
     else
-      ''
+      ' '
     end
 
   end
 
   def buzz(number)
     if (number % 5).zero? || (number.to_s.include? '5')
-      'buzz'
+      ' buzz'
     else
-      ''
+      ' '
     end
   end
 end
+
