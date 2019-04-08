78fafffb867d082058b29ba744ef6e42f5a57d64
sbx_user1051 <sbx_user1051@169.254.237.165>
2019-04-08 14:10:01 +0000

    Mon Apr 08 14:10:01 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -6,31 +6,36 @@ class FizzBuzz
   def fizz_buzz(number)
     fizz_str = nil
     buzz_str = nil
-    if fizz_buzz?(number)
-      'fizz buzz'
-    elsif fizz?(number)
-      'fizz'
-    elsif buzz?(number)
-      'buzz'
-    else
+
+    fizz_buzz(number)
+    fizz(number)
+    buzz(number)
+    if fizz.length + buzz.length == 0
       number.to_s
+    else
+      (fizz_str + ' ' + buzz_str).strip!
     end
   end
 
-  def fizz_buzz?(number)
+  def fizz_buzz(number)
     (number % 15).zero? ||
       (number.to_s.include? '3') && (number.to_s.include? '5')
+
   end
 
-  def fizz?(number)
+  def fizz(number)
     if (number % 3).zero? || (number.to_s.include? '3')
-     fizz_num = 1
+     fizz_str
+    end
   end
 
   def buzz?(number)
-    (number % 5).zero? || (number.to_s.include? '5')
+    if (number % 5).zero? || (number.to_s.include? '5')
+      buzz_str = 'buzz'
+    end
   end
 end
+
 
 
 
