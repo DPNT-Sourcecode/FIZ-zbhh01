7b734f8f639510c4597681fd67754c134c8c7c68
sbx_user1051 <sbx_user1051@169.254.5.129>
2019-04-09 20:30:33 +0000

    Tue Apr 09 20:30:33 UTC 2019

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '~> 2.2.2'
 gem 'tdl-client-ruby', '0.25.1'
 gem 'java-properties', '0.2.0'
 gem 'unirest', '1.1.2'
+gem 'rspec', '3.8.0'
 
 gem 'require_all', '2.0.0'
 
@@ -12,4 +13,3 @@ gem 'simplecov-csv', require: false, group: :test
 
 gem 'minitest', '~> 5.8.3'
 gem 'rake'
-gem 'rubocop-performance'
--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,8 +1,74 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
 
+  attr_reader :item_prices, :special_offers, :total_price, :sorted_basket
+
   def checkout(skus)
-    raise 'Not implemented'
+    set_up
+    if !check_valid(skus)
+      return -1
+    end
+    sort_basket(skus)
+    check_specials
+    add_up_basket
   end
 
+  def set_up
+    @item_prices = { 'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15 }
+    @special_offers = { 'A' => [3,130], 'B' => [2,45]}
+    @total_price = 0
+    @sorted_basket = []
+  end
+
+  def check_valid(basket)
+    basket_valid = true
+    x = 0
+    while basket_valid && (x < basket.chars.length) do
+      if !@item_prices.include?(basket.chars[x])
+        basket_valid = false
+      end
+      x = x + 1
+    end
+    basket_valid
+  end
+
+  def sort_basket(skus)
+    basic_items(skus)
+    check_specials
+  end
+
+  def basic_items(skus)
+    @item_prices.each do | item, price |
+      item_array = []
+      item_array[0] = item
+      item_array[1] = skus.chars.count(item)
+      item_array[2] = price
+      @sorted_basket << item_array
+    end
+  end
+
+  def check_specials
+    @sorted_basket.each do | item, qty, price |
+      if @special_offers.include?(item)
+        so_qty = @special_offers[item][0]
+        so_price = @special_offers[item][1]
+        if qty >= so_qty
+          x = qty.div(so_qty)
+          qty = qty.modulo(so_qty) # Remainder
+          so_qty = x
+          item_array = ['O', so_qty, so_price]
+          @sorted_basket << item_array
+          @sorted_basket
+        end
+      end
+    end
+  end
+
+  def add_up_basket
+    p @sorted_basket
+    @sorted_basket.each do | item, qty, price |
+      @total_price += (qty * price)
+    end
+    @total_price
+  end
 end
